f7c2dabc536d8acbefe3b611debf1b179396f4e8
Kenneth Crawford <kmc059000@gmail.com>
2017-07-21 16:40:12 -0500

    implemented dojo with pleasing parameters

--- Dojo/FractalForest/Script.fsx
+++ Dojo/FractalForest/Script.fsx
@@ -3,7 +3,7 @@ open System.Drawing
 open System.Windows.Forms
 
 // Create a form to display the graphics
-let width, height = 500, 500         
+let width, height = 800, 800
 let form = new Form(Width = width, Height = height)
 let box = new PictureBox(BackColor = Color.White, Dock = DockStyle.Fill)
 let image = new Bitmap(width, height)
@@ -12,7 +12,7 @@ let graphics = Graphics.FromImage(image)
 //at the expense of speed. Uncomment it if you want
 //more beautiful images, even if it's slower.
 //Thanks to https://twitter.com/AlexKozhemiakin for the tip!
-//graphics.SmoothingMode <- System.Drawing.Drawing2D.SmoothingMode.HighQuality
+graphics.SmoothingMode <- System.Drawing.Drawing2D.SmoothingMode.HighQuality
 let brush = new SolidBrush(Color.FromArgb(0, 0, 0))
 
 box.Image <- image
@@ -38,19 +38,52 @@ let drawLine (target : Graphics) (brush : Brush)
     let destination = new PointF((single)x_end, (single)(y_end |> flip))
     let pen = new Pen(brush, (single)width)
     target.DrawLine(pen, origin, destination)
+    x_end, y_end
 
 let draw x y angle length width = 
     drawLine graphics brush x y angle length width
 
 let pi = Math.PI
 
-// Now... your turn to draw
-// The trunk
-draw 250. 50. (pi*(0.5)) 100. 4.
-let x, y = endpoint 250. 50. (pi*(0.5)) 100.
-// first and second branches
-draw x y (pi*(0.5 + 0.3)) 50. 2.
-draw x y (pi*(0.5 - 0.4)) 50. 2.
+let rand = System.Random()
+
+let maxDepth = 15
+let startLength = 90.
+let startWidth = 20.
+
+let lengthReductionRate = 0.8
+let widthReductionRate = 0.7
+
+let angleMidpoint = 0.25
+let maxAngle = 0.35
+let terminateChance = 0.15
+
+let getAngle x = 
+    let r = rand.NextDouble () 
+    angleMidpoint + (r * maxAngle)
+    
+
+let terminateBranch (depth:int) =
+    let fDepth = float depth
+    let fMaxDepth = float maxDepth
+    let r = rand.NextDouble ()
+    let threshold = (fDepth * terminateChance / fMaxDepth)
+    r < threshold
+
+let getLength length (depth:int) =
+    let fDepth = float depth
+    let r = rand.NextDouble() * 0.05
+    length * (lengthReductionRate + r)
+
+let rec branch depth x y angle length width =
+    if depth = maxDepth then ()
+    else if (terminateBranch depth) then ()
+    else
+        let x', y' = draw x y angle length width
+        branch (depth + 1) x' y' (angle - getAngle ()) (getLength length depth) (width * widthReductionRate)
+        branch (depth + 1) x' y' (angle + getAngle ()) (getLength length depth) (width * widthReductionRate)
+
+branch 0 400. 150. (pi*(0.5)) startLength startWidth
 
 form.ShowDialog()
 
